e7901a0a2151ae8d50ff5fea4b69fb7c6fd0cacc
Vilkov-Kirill <99217802+Vilkov-Kirill@users.noreply.github.com>
2022-03-06 19:47:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,6 +4,6 @@
 
 int cbinsearch(int *arr, int size, int value) {
     int quan = 0;
-    for (int i = 0; i < size; i++) { if (*(arr + i) == value) quan += 1; }
+    for (int i = 0; i < size; i++) { if (*(arr + i) == value) { quan += 1; } };
     return quan;
 }
