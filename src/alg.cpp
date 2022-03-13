46f9ac06f810084e0bcf0619452742c91e6e6ca1
Vilkov-Kirill <99217802+Vilkov-Kirill@users.noreply.github.com>
2022-03-13 21:47:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,11 +3,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-    if (size == 1) {
-        if (*arr == value) { return 1; }
-        else { return 0; }
+    if (size == 1) 
+    {
+        if (*arr == value)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
-    else {
+    else
+    {
         return cbinsearch(arr, size / 2, value) +
                cbinsearch(arr + size / 2, size / 2 + (size % 2), value);
     }
